1c7623910f801ea9229602ad689f32075fc3be62
Kiirya Arnold <32293740+arnoldkiirya1@users.noreply.github.com>
2023-05-18 20:28:27 +0300

    Update emily.py

--- emily.py
+++ emily.py
@@ -1,9 +1,6 @@
-#!/bin/bash
-
 def print_birthday_message(name):
     message = f"Happy Birthday to {name}!"
     print(message)
 
 # Call the function to print the birthday message
 print_birthday_message("Emily")
-
