73d662cd4dcf27bc20d485a1e31e5bc9cc3999f7
Kiirya Arnold <kiiryarnold@gmail.com>
2023-05-18 11:26:36 -0700

    Others

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+    dlistint_t *current, *temp;
+    unsigned int i;
+
+    if (head == NULL || *head == NULL)
+        return -1;
+
+    current = *head;
+    if (index == 0)
+    {
+        *head = current->next;
+        if (*head != NULL)
+            (*head)->prev = NULL;
+        free(current);
+        return 1;
+    }
+
+    for (i = 0; i < index && current != NULL; i++)
+        current = current->next;
+
+    if (current == NULL)
+        return -1;
+
+    temp = current->prev;
+    temp->next = current->next;
+    if (current->next != NULL)
+        current->next->prev = temp;
+    free(current);
+
+    return 1;
+}
+
