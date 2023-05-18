ff2d02efbea39178b97498d28265f61abc2a6f0b
Kiirya Arnold <kiiryarnold@gmail.com>
2023-05-18 10:22:43 -0700

    Added

---
+++ emily.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+def print_birthday_message(name):
+    message = f"Happy Birthday to {name}!"
+    print(message)
+
+# Call the function to print the birthday message
+print_birthday_message("Emily")
+
